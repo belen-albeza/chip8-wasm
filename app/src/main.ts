d67c2c3fb2c2f13e147786478ec54cbc1957ba9b
Belén Albeza <belen@hey.com>
2024-07-26 17:35:28 +0200

    Setup wiring between web app and wasm

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,1 +1,46 @@
-import "./style.css";
+import { loadRom } from "chip8";
+
+const ROMS = [{ name: "poker.ch8", url: "/roms/poker.ch8" }];
+
+main();
+
+function main() {
+  setupRomSelector(ROMS);
+  runRom(ROMS[0].url).then((vm) => {
+    vm.run();
+  });
+}
+
+function setupRomSelector(roms: { name: string; url: string }[]) {
+  const selectEl = document.querySelector("#chip8-rom-selector");
+  for (const { name, url } of roms) {
+    const option = new Option();
+    option.value = url;
+    option.innerHTML = name;
+    option.defaultSelected = name === roms[0]?.name;
+
+    selectEl?.appendChild(option);
+  }
+}
+
+async function fetchRom(url: string) {
+  try {
+    const response = await fetch(url);
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function runRom(url: string) {
+  const file = await fetchRom(url);
+  if (!file) {
+    throw new Error(`Could not load rom: ${url}`);
+  }
+
+  const buffer = await file.arrayBuffer();
+  const rom = new Uint8Array(buffer);
+
+  const vm = loadRom(rom);
+  return vm;
+}
--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -3,3 +3,8 @@ declare module "*.svg" {
   const content: SVGElement;
   export default content;
 }
+
+declare module "*.ch8" {
+  const content: string;
+  export default content;
+}
