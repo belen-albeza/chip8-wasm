7050d5b0897a8748882ec6954a6fcd8707789d1c
Belén Albeza <belen@hey.com>
2024-07-27 15:28:08 +0200

    Share array buffer between Wasm and JS

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,43 @@
-import { loadRom } from "chip8";
+import wasmInit, { loadRom, Emu } from "chip8";
 
 const ROMS = [{ name: "poker.ch8", url: "/roms/poker.ch8" }];
+const DISPLAY_LEN = 64 * 32;
 
 main();
 
-function main() {
+async function main() {
+  const wasm = await wasmInit();
+
   setupRomSelector(ROMS);
-  runRom(ROMS[0].url).then((vm) => {
-    vm.run();
-  });
+
+  const emu = await loadRomInEmu(ROMS[0].url);
+  emu.run();
+
+  const sharedBuffer = new Uint8Array(wasm.memory.buffer);
+  const canvas = document.querySelector<HTMLCanvasElement>("#chip8-canvas");
+  const ctx = canvas?.getContext("2d");
+  if (!ctx || !canvas) {
+    throw new Error("Valid canvas not found");
+  }
+  const imageData = ctx.createImageData(canvas.width, canvas.height);
+
+  updateCanvas(ctx, imageData, sharedBuffer);
+}
+
+function updateCanvas(
+  ctx: CanvasRenderingContext2D,
+  canvasData: ImageData,
+  buffer: Uint8Array
+) {
+  const outputPointer = Emu.display_buffer();
+  const imageData = buffer.slice(
+    outputPointer,
+    outputPointer + 4 * DISPLAY_LEN
+  );
+  canvasData.data.set(imageData);
+  ctx.putImageData(canvasData, 0, 0);
+
+  requestAnimationFrame(() => updateCanvas(ctx, canvasData, buffer));
 }
 
 function setupRomSelector(roms: { name: string; url: string }[]) {
@@ -32,7 +61,7 @@ async function fetchRom(url: string) {
   }
 }
 
-async function runRom(url: string) {
+async function loadRomInEmu(url: string) {
   const file = await fetchRom(url);
   if (!file) {
     throw new Error(`Could not load rom: ${url}`);
@@ -41,6 +70,6 @@ async function runRom(url: string) {
   const buffer = await file.arrayBuffer();
   const rom = new Uint8Array(buffer);
 
-  const vm = loadRom(rom);
-  return vm;
+  const emu = loadRom(rom);
+  return emu;
 }
